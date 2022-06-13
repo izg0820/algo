af06ba9786b5f3f2e6721644efb757ede540c6f8
izg0820 <izg0820@naver.com>
2022-06-13 22:48:31 +0900

    프로그래머스 오픈채팅방(해쉬) 추가

--- src/programmers/OpenChat.java
+++ src/programmers/OpenChat.java
@@ -3,11 +3,10 @@ package programmers;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+//https://programmers.co.kr/learn/courses/30/lessons/42888
 public class OpenChat {
 
     final String leave = "님이 나갔습니다.";
@@ -15,56 +14,31 @@ public class OpenChat {
 
     public String[] solution(String[] record) {
         HashMap<String, String> ids = new HashMap<>();
-        List<Log> logs = new ArrayList<>();
+        List<String> logs = new ArrayList<>();
 
         for (String r : record) {
             String[] temp = r.split(" ");
-            String action = temp[0], uid = temp[1];
 
-            if ("Enter".equals(action)) {
-                if (ids.containsKey(uid)) {
-                    logs.stream().forEach(p -> p.changeName(uid, ids.get(uid), temp[2]));
-                }
-                ids.put(uid, temp[2]);
-                logs.add(new Log(uid, temp[2] + enter));
-            } else if ("Leave".equals(action)) {
-                logs.add(new Log(uid ,ids.get(uid) + leave));
-            } else if ("Change".equals(action)) {
-                logs.stream().forEach(p -> p.changeName(uid, ids.get(uid), temp[2]));
-                ids.put(uid, temp[2]);
+            if ("Enter".equals(temp[0])) {
+                ids.put(temp[1], temp[2]);
+                logs.add(temp[1] + enter);
+            } else if ("Leave".equals(temp[0])) {
+                logs.add(temp[1] + leave);
+            } else if ("Change".equals(temp[0])) {
+                ids.put(temp[1], temp[2]);
             }
         }
 
         String[] answer = new String[logs.size()];
         for (int i = 0; i < logs.size(); i++) {
-            answer[i] = logs.get(i).getContext();
+            String context = logs.get(i);
+            int idx = context.indexOf("님");
+            String id = context.substring(0, idx);
+            answer[i] = ids.get(id) + "님이 " +context.split(" ")[1];
         }
 
         return answer;
     }
-
-
-    public class Log {
-
-        private String id;
-        private String context;
-
-        public void changeName(String id, String before, String after) {
-            if (this.id.equals(id)) {
-                context = context.replace(before, after);
-            }
-        }
-
-        public Log(String id, String context) {
-            this.id = id;
-            this.context = context;
-        }
-
-        public String getContext() {
-            return context;
-        }
-    }
-
 
     @Test
     public void result() {
