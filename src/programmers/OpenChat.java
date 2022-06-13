2b5c8a5f5d012acdb12eb9993c5bcb3b7703d2c5
이재현 <izg0820>
2022-06-13 17:57:54 +0900

    프로그래머스 오픈채팅방(??) 임시

--- src/programmers/OpenChat.java
+++ src/programmers/OpenChat.java
@@ -14,7 +14,7 @@ public class OpenChat {
     final String enter = "님이 들어왔습니다.";
 
     public String[] solution(String[] record) {
-        Ids ids = new Ids();
+        HashMap<String, String> ids = new HashMap<>();
         List<Log> logs = new ArrayList<>();
 
         for (String r : record) {
@@ -22,24 +22,24 @@ public class OpenChat {
             String action = temp[0], uid = temp[1];
 
             if ("Enter".equals(action)) {
-                if (ids.isExists(uid)) {
-                    logs.stream().forEach(p -> p.changeName(uid, ids.getName(uid), temp[2]));
-                    ids.changeName(uid, temp[2]);
-                } else {
-                    ids.add(uid, temp[2]);
+                if (ids.containsKey(uid)) {
+                    logs.stream().forEach(p -> p.changeName(uid, ids.get(uid), temp[2]));
                 }
+                ids.put(uid, temp[2]);
                 logs.add(new Log(uid, temp[2] + enter));
             } else if ("Leave".equals(action)) {
-                logs.add(new Log(uid ,ids.getName(uid) + leave));
+                logs.add(new Log(uid ,ids.get(uid) + leave));
             } else if ("Change".equals(action)) {
-                logs.stream().forEach(p -> p.changeName(uid, ids.getName(uid), temp[2]));
-                ids.changeName(uid, temp[2]);
+                logs.stream().forEach(p -> p.changeName(uid, ids.get(uid), temp[2]));
+                ids.put(uid, temp[2]);
             }
         }
+
         String[] answer = new String[logs.size()];
         for (int i = 0; i < logs.size(); i++) {
             answer[i] = logs.get(i).getContext();
         }
+
         return answer;
     }
 
@@ -65,26 +65,6 @@ public class OpenChat {
         }
     }
 
-    public class Ids {
-
-        private HashMap<String, String> info = new HashMap<>();
-
-        public void add(String id, String name) {
-            info.put(id, name);
-        }
-
-        public boolean isExists(String id) {
-            return info.containsKey(id);
-        }
-
-        public void changeName(String id, String name) {
-            info.put(id, name);
-        }
-
-        public String getName(String id) {
-            return info.get(id);
-        }
-    }
 
     @Test
     public void result() {
